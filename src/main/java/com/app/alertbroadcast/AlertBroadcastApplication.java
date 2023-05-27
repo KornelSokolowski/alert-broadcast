3e23542a148b14ce65a2cbf1b562684cf3eb9e73
Kornel Sokolowski <example@example.com>
2023-05-27 13:14:23 +0200

    initial commit

---
+++ src/main/java/com/app/alertbroadcast/AlertBroadcastApplication.java
@@ -0,0 +1,13 @@
+package com.app.alertbroadcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertBroadcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertBroadcastApplication.class, args);
+	}
+
+}
